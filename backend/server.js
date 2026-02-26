f4e96d546f1d9ee1446c70be71bfe2b94208632f
vaishnavi sondkar <vaishnavisondkar266@gmail.com>
2026-02-26 15:45:19 +0530

    Fixed PORT binding

--- backend/server.js
+++ backend/server.js
@@ -130,6 +130,8 @@ app.get("/", (req, res) => {
 });
 // ---------------- START SERVER ----------------
 // ---------------- START SERVER ----------------
-app.listen(process.env.PORT || 8080, "0.0.0.0", () => {
-  console.log(`🚀 Server running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
